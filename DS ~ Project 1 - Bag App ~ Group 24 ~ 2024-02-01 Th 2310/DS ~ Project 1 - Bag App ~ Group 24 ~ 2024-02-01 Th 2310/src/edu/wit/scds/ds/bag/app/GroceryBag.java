a9177d9948da9215c8972d8b5bc329faaa1b1f2c
folajimiiatwit <folajimii@wit.edu>
2024-02-05 13:01:07 -0500

    Added the 3 classes:GroceryItem,GroceryBag,GroceryBagger

---
+++ DS ~ Project 1 - Bag App ~ Group 24 ~ 2024-02-01 Th 2310/DS ~ Project 1 - Bag App ~ Group 24 ~ 2024-02-01 Th 2310/src/edu/wit/scds/ds/bag/app/GroceryBag.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package edu.wit.scds.ds.bag.app;
+
+/**
+ * 
+ */
+public class GroceryBag {
+
+}
---
+++ DS ~ Project 1 - Bag App ~ Group 24 ~ 2024-02-01 Th 2310/DS ~ Project 1 - Bag App ~ Group 24 ~ 2024-02-01 Th 2310/src/edu/wit/scds/ds/bag/app/GroceryBagger.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.wit.scds.ds.bag.app;
+
+/**
+ * 
+ */
+public class GroceryBagger {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
